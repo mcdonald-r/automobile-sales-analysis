d2f0617cc9040648af8a1db449102b87e3e37dd9
mcdonald-r <work.robertmcdonald@gmail.com>
2026-03-18 19:56:26 -0700

    added 3 queries for sub question 3

--- automobile_queries.sql
+++ automobile_queries.sql
@@ -97,12 +97,41 @@ having round((sum(case when "STATUS" in ('Cancelled', 'Disputed', 'On Hold')
 	then "SALES" else 0 end) / sum("SALES"))::numeric * 100, 2) > 0
 order by at_risk_percentage desc;
 
--- 
-select distinct "CUSTOMERNAME", sum("SALES") as total_sales, count("ORDERNUMBER")
+-- Customers who have at risk orders displaying total sales at risk and how many orders are at risk
+select "CUSTOMERNAME", sum("SALES") as total_sales, count("ORDERNUMBER") as total_at_risk_orders
 from auto_sales_data
 where "STATUS" in ('Cancelled', 'Disputed', 'On Hold')
 group by "CUSTOMERNAME"
 order by total_sales desc;
+
+-- Compares a customer's at risks sales to their completed sales total and calculates what percent of their sales
+-- are at risk
+select "CUSTOMERNAME", sum(case when "STATUS" in ('Cancelled', 'Disputed', 'On Hold') 
+	then "SALES" else 0 end) as at_risk_sales, 
+sum(case when "STATUS" in ('Shipped', 'Resolved') 
+	then "SALES" else 0 end) as completed_sales, 
+round((sum(case when "STATUS" in ('Cancelled', 'Disputed', 'On Hold') 
+	then "SALES" else 0 end) / sum("SALES"))::numeric * 100, 2) as at_risk_percentage 
+from auto_sales_data 
+group by "CUSTOMERNAME"
+having round((sum(case when "STATUS" in ('Cancelled', 'Disputed', 'On Hold') 
+	then "SALES" else 0 end) / sum("SALES"))::numeric * 100, 2) > 0
+order by at_risk_percentage desc;
+
+-- Breaks down each customer's at-risk orders by category to specify what types of problems are happening repeatedly
+-- with their orders
+SELECT 
+    "CUSTOMERNAME",
+    COUNT("ORDERNUMBER") AS total_at_risk_orders,
+    SUM(CASE WHEN "STATUS" = 'Cancelled' THEN 1 ELSE 0 END) AS cancelled,
+    SUM(CASE WHEN "STATUS" = 'Disputed' THEN 1 ELSE 0 END) AS disputed,
+    SUM(CASE WHEN "STATUS" = 'On Hold' THEN 1 ELSE 0 END) AS on_hold,
+    SUM("SALES") AS total_at_risk_sales
+FROM auto_sales_data
+WHERE "STATUS" IN ('Cancelled', 'Disputed', 'On Hold')
+GROUP BY "CUSTOMERNAME"
+HAVING COUNT("ORDERNUMBER") > 1
+ORDER BY total_at_risk_orders DESC;
 
 -- SCRATCH WORK
 
